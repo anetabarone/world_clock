bc1449a8f4e7c9ddb1f26316b924b5a2aee2df7f
anetabarone <anetabarone@yahoo.com>
2023-03-10 07:11:39 +0100

    js beginning

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = document.querySelector("#los-angeles .date");
+//or let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = document.querySelector("#los-angeles .time");
+losAngelesDateElement.innerHTML = "August 15th 2023";
+losAngelesTimeElement = "1:48:15<small>AM</small>";
