d12468dabd89bfbc94834c5af852764808b6c14e
anetabarone <anetabarone@yahoo.com>
2023-03-12 10:34:41 +0100

    added local time option

--- javascript/index.js
+++ javascript/index.js
@@ -25,6 +25,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -36,7 +39,7 @@ function updateCity(event) {
         </div>
             <div class="time">${cityTime.format(
               "h:m:ss"
-            )}<small>${cityTime.format("A")} </small></div>
+            )} <small>${cityTime.format("A")}</small></div>
         </div>
 `;
 }
